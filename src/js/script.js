382a6561ac7c0b8ccc000e61c84eb8e204781e4c
Roman <roman985475612@gmail.com>
2021-06-16 11:39:00 +0300

    Project completed

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,78 @@ $( function() {
         }
     } )
 } )
+
+window.addEventListener('DOMContentLoaded', () => {
+    // Back to top
+    document.getElementById('backToTop').addEventListener('click', e => {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    })
+
+    // Scroll Spy
+	let menu = document.getElementById('menu');
+    let links = menu.querySelectorAll('.menu-link')
+    let scrollTimeout;
+    let logoBtn = document.getElementById('logoBtn')
+    let aboutBtn = document.getElementById('aboutBtn')
+    let contactBtn = document.getElementById('contactBtn')
+
+    logoBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        scrollToTarget(e.target.hash)
+    })
+
+    aboutBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        scrollToTarget(e.target.hash)
+    })
+
+    contactBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        let target = e.target
+        if (e.target.classList.contains('scroll-icon')) {
+            target = e.target.parentNode
+        }
+        console.log(target)
+        scrollToTarget(target.hash)
+    })
+
+	window.addEventListener('scroll', function(){
+		clearTimeout(scrollTimeout);
+		scrollTimeout = setTimeout(onScroll, 200);
+	});
+
+	menu.addEventListener('click', (e) => {
+		let link = e.target;
+
+		if (link.classList.contains('menu-link')) {
+			e.preventDefault();
+            menu.querySelector('.active').classList.remove('active');
+            link.parentNode.classList.add('active');
+            scrollToTarget(link.hash);
+		}
+	});
+	
+    function scrollToTarget(id){
+		let target = document.querySelector(id)
+
+		if (target !== null) {
+			let pos = target.offsetTop - 64
+			window.scrollTo({top: pos, behavior: 'smooth'})
+		}
+	}
+
+    function onScroll(){
+		let pos = window.pageYOffset;
+		for (let i = links.length - 1; i >= 0; i--) {
+			let link = links[i];
+			let target = document.querySelector(link.hash);
+			
+			if ((pos + window.innerHeight / 2) > target.offsetTop) {
+				menu.querySelector('.active').classList.remove('active');
+				link.parentNode.classList.add('active');
+				break;
+			}
+		}
+	}
+
+})
